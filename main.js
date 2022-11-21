7edd9497ce642088348dfabc77ffdd59d311b219
Flaviabel <flaviamanfredi521@gmail.com>
2022-11-21 10:14:45 +0100

    function remove

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 let tarjetas = document.querySelectorAll('.panel');
+let h3= document.querySelectorAll('h3')
 
-function abrir() {
-   this.classList.toggle('abrir');
+for (let i = 0; i < tarjetas.length; i++) {  
+   tarjetas[i].addEventListener('click', function abrir(){
+      removeclass();
+      tarjetas[i].classList.add('abrir');
+      h3[i].classList.add('block');
+
+   });
+   function removeclass(){
+      for (let i = 0; i < tarjetas.length; i++) {
+         tarjetas[i].classList.remove('abrir');
+         h3[i].classList.remove('block');
+      }
+}
 }
 
-for (let i = 0; i < tarjetas.length; i++) {
-   tarjetas[i].addEventListener('click', abrir);
-}
 
